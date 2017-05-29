347b843fa317c51d3d90d94a5657cb3c445b4625
Ian <ian (at) ianrobinson.net>
2017-05-28 19:27:26 -0700

    calcBrain.js now includes add(), subtract(), divide() and multiply() functions

--- js/calcBrain.js
+++ js/calcBrain.js
@@ -1,15 +1,29 @@
 
-
-var calculatorValue, previousNum = "", stringyNum = "";
+var stringyNum = '', calculatorValue, previousNum = "";
 var numbers = "1234567890.".split("");
 
 function updateDisplay(stringyNum) {
-  // console.log(stringyNum);
   $('th#display').replaceWith('<th colspan="4" id="display"><p align="right">' + stringyNum + '</p></th>');
 }
 
 function stringIt(stringyNum, buttonPressed) {
+  // console.log("stringingIt");
   stringyNum += buttonPressed;
-  console.log(stringyNum, "<-string it")
   updateDisplay(stringyNum);
+}
+
+function add(previousNum, stringyNum) {
+  return parseFloat(previousNum) + parseFloat(stringyNum)
+}
+
+function subtract(previousNum, stringyNum) {
+  return parseFloat(previousNum) - parseFloat(stringyNum)
+}
+
+function divide(previousNum, stringyNum) {
+  return parseFloat(previousNum) / parseFloat(stringyNum)
+}
+
+function multiply(previousNum, stringyNum) {
+  return parseFloat(previousNum) * parseFloat(stringyNum)
 }
