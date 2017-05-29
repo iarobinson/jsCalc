273cdd16ae00673f2cb5c379c94ed145b178f98e
Ian <ian (at) ianrobinson.net>
2017-05-28 19:04:24 -0700

    Add clear differentiation

--- js/calcBrain.js
+++ js/calcBrain.js
@@ -1,11 +1,15 @@
-//
 
-var calculatorValue, stringyNum = "", previousNum = "";
 
-function updateDisplay(num) {
-  // console.log(num, " <- num");
-  // if (num === 'add') {
-  //   previousNum =
-  // }
+var calculatorValue, previousNum = "", stringyNum = "";
+var numbers = "1234567890.".split("");
+
+function updateDisplay(stringyNum) {
+  // console.log(stringyNum);
   $('th#display').replaceWith('<th colspan="4" id="display"><p align="right">' + stringyNum + '</p></th>');
+}
+
+function stringIt(stringyNum, buttonPressed) {
+  stringyNum += buttonPressed;
+  console.log(stringyNum, "<-string it")
+  updateDisplay(stringyNum);
 }
