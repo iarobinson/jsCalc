2302c80ba038f2a8ff47209617140e1ec3e484bb
Ian <ian (at) ianrobinson.net>
2017-06-14 03:02:54 -0700

    Add the ability to chain equals after the equals operand is run

--- js/calcBrain.js
+++ js/calcBrain.js
@@ -1,7 +1,7 @@
 
 var stringyNum = '', previousNum = '', operand = '';
-var numbers = "1234567890".split("");
-var operandSelected = ['add', 'subtract', 'multiply', 'divide', 'equals'];
+var numbers = '1234567890'.split('');
+var operandSelected = ['add', 'subtract', 'multiply', 'divide'];
 
 function updateDisplay(stringyNum) {
   $('th#display').replaceWith('<th colspan="4" id="display"><p align="right">' + stringyNum.substring(0, 10) + '</p></th>');
@@ -32,12 +32,19 @@ function runTheNumbers(buttonPressed) {
     }
   } else if (buttonPressed === 'equals') {
     stringyNum = runCalc();
-    previousNum = stringyNum;
-    operand = '';
     updateDisplay(stringyNum);
+    previousNum = stringyNum;
+    stringyNum = '';
+    operand = '';
+
   } else {
     updateDisplay(stringyNum);
   }
+
+  console.log(previousNum, "<-previousNum");
+  console.log(operand, "<-operand");
+  console.log(stringyNum, "<-stringyNum");
+  console.log("--------");
 }
 
 function runCalc() {
@@ -54,6 +61,8 @@ function runCalc() {
   } else if (operand === 'divide') {
     result = parseFloat(previousNum) / parseFloat(stringyNum);
     return result.toString();
+  } else {
+    return previousNum;
   }
 }
 
